df16b37bc8fd70bc88be7b90529ead3158d022db
gracefulghost88 <maeng.jieon@gmail.com>
2025-04-13 18:43:32 +0900

    feat: winston logger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import { loggerConfig } from './infrastructure/framework/logger/logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(loggerConfig),
+    bufferLogs: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
