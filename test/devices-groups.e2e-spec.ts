c07e13382aa82b33caff039a501db6a6428bdc10
gracefulghost88 <maeng.jieon@gmail.com>
2025-04-29 15:10:21 +0900

    fix: 장비 그룹 중복 등록 테스트 분리

--- test/devices-groups.e2e-spec.ts
+++ test/devices-groups.e2e-spec.ts
@@ -7,7 +7,8 @@ import {
 } from '../src/shared/error-code/device-group.error-code';
 import { DeviceGroupDto } from '../src/presentation/device-group/dto/create-device-group.response.dto';
 import { RemoveDeviceGroupUsecase } from '../src/usecase/remove-device-group.usecase';
-import { RemoveDeviceGroupDto } from 'src/usecase/dto/remove-device-group.dto';
+import { RemoveDeviceGroupDto } from '../src/usecase/dto/remove-device-group.dto';
+import { CreateDeviceGroupUsecase } from '../src/usecase/create-device-group.usecase';
 
 beforeAll(async () => {
   if (server.status !== 'ready') {
@@ -21,46 +22,101 @@ afterAll(async () => {
 
 describe('DevicesGroups E2E Test', () => {
   describe('POST 장비 그룹 등록', () => {
-    let deviceGroup = null;
+    describe('장비 그룹 신규 등록', () => {
+      let removeDeviceGroupDto: RemoveDeviceGroupDto = null;
 
-    afterAll(async () => {
-      const removeDeviceGroupUsecase =
-        server.moduleRef?.get<RemoveDeviceGroupUsecase>(
-          RemoveDeviceGroupUsecase,
-        );
+      afterAll(async () => {
+        const removeDeviceGroupUsecase =
+          server.moduleRef?.get<RemoveDeviceGroupUsecase>(
+            RemoveDeviceGroupUsecase,
+          );
 
-      if (deviceGroup !== null) {
-        await removeDeviceGroupUsecase.execute(deviceGroup);
-      }
+        if (removeDeviceGroupDto !== null) {
+          await removeDeviceGroupUsecase.execute(removeDeviceGroupDto);
+        }
+      });
+
+      it.each`
+        testName                                 | deviceGroupSerial | responseStatus | errorInfo
+        ${'[400] deviceGroupSerial is missing.'} | ${undefined}      | ${400}         | ${MISSING_DEVICE_GROUP_SERIAL.ErrorInfo}
+        ${'[201] OK'}                            | ${'A5'}           | ${201}         | ${undefined}
+      `(
+        '$testName',
+        async ({ deviceGroupSerial, responseStatus, errorInfo }) => {
+          const response = await request(server.app.getHttpServer())
+            .post(`/devices-groups`)
+            .set('Content-Type', 'application/json')
+            .send({ deviceGroupSerial });
+
+          expect(response.status).toBe(responseStatus);
+
+          if (response.status === 400) {
+            console.log(response.body);
+            expect(response.body).toEqual(errorInfo);
+          } else if (response.status === 201) {
+            expect(response.body.msg).toBe('success');
+            expect((response.body.data as DeviceGroupDto).serialNumber).toBe(
+              deviceGroupSerial,
+            );
+
+            const deviceGroupDto = response.body.data as DeviceGroupDto;
+            removeDeviceGroupDto = {
+              serialNumber: deviceGroupDto.serialNumber,
+            } as unknown as RemoveDeviceGroupDto;
+          }
+        },
+      );
     });
 
-    it.each`
-      testName                                    | deviceGroupSerial | responseStatus | errorInfo
-      ${'[400] deviceGroupSerial is missing.'}    | ${undefined}      | ${400}         | ${MISSING_DEVICE_GROUP_SERIAL.ErrorInfo}
-      ${'[400] device group does exist already.'} | ${'A7'}           | ${400}         | ${ALREADY_EXIST_DEVICE_GROUP.ErrorInfo}
-      ${'[201] OK'}                               | ${'A5'}           | ${201}         | ${undefined}
-    `('$testName', async ({ deviceGroupSerial, responseStatus, errorInfo }) => {
-      const response = await request(server.app.getHttpServer())
-        .post(`/devices-groups`)
-        .set('Content-Type', 'application/json')
-        .send({ deviceGroupSerial });
+    describe('장비 그룹 중복 등록', () => {
+      let removeDeviceGroupDto: RemoveDeviceGroupDto = null;
 
-      expect(response.status).toBe(responseStatus);
+      beforeAll(async () => {
+        const createDeviceGroupUsecase =
+          server.moduleRef?.get<CreateDeviceGroupUsecase>(
+            CreateDeviceGroupUsecase,
+          );
+        if (createDeviceGroupUsecase) {
+          const deviceGroup = await createDeviceGroupUsecase.execute({
+            serialNumber: 'A5',
+          });
 
-      if (response.status === 400) {
-        console.log(response.body);
-        expect(response.body).toEqual(errorInfo);
-      } else if (response.status === 201) {
-        expect(response.body.msg).toBe('success');
-        expect((response.body.data as DeviceGroupDto).serialNumber).toBe(
-          deviceGroupSerial,
-        );
+          removeDeviceGroupDto = {
+            serialNumber: deviceGroup.serialNumber,
+          } as unknown as RemoveDeviceGroupDto;
+        }
+      });
 
-        const deviceGroupDto = response.body.data as DeviceGroupDto;
-        deviceGroup = {
-          serialNumber: deviceGroupDto.serialNumber,
-        } as unknown as RemoveDeviceGroupDto;
-      }
+      afterAll(async () => {
+        const removeDeviceGroupUsecase =
+          server.moduleRef?.get<RemoveDeviceGroupUsecase>(
+            RemoveDeviceGroupUsecase,
+          );
+
+        if (removeDeviceGroupDto !== null) {
+          await removeDeviceGroupUsecase.execute(removeDeviceGroupDto);
+        }
+      });
+
+      it.each`
+        testName                                    | deviceGroupSerial | responseStatus | errorInfo
+        ${'[400] device group does exist already.'} | ${'A7'}           | ${400}         | ${ALREADY_EXIST_DEVICE_GROUP.ErrorInfo}
+      `(
+        '$testName',
+        async ({ deviceGroupSerial, responseStatus, errorInfo }) => {
+          const response = await request(server.app.getHttpServer())
+            .post(`/devices-groups`)
+            .set('Content-Type', 'application/json')
+            .send({ deviceGroupSerial });
+
+          expect(response.status).toBe(responseStatus);
+
+          if (response.status === 400) {
+            console.log(response.body);
+            expect(response.body).toEqual(errorInfo);
+          }
+        },
+      );
     });
   });
 });
